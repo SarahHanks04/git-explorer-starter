3682a2357de54c89c99579c435c966568e8ada6c
SarahHanks04 <sarahhanks04@gmail.com>
2024-08-22 22:53:42 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -5,17 +5,24 @@ import AboutUs from "./components/about";
 import Users from "./components/users";
 import Navbar from "./components/navbar";
 import NotFound from "./components/notFound";
+import UserProfile from "./components/userProfile";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+
         <Route path="/" element={<Navbar />}>
+
           <Route exact path="/" element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/users" element={<Users />} />
+          <Route element={<UserProfile />} path="/users/user" />
+
         </Route>
+
         <Route element={<NotFound />} path="*" />
+
       </Routes>
     </div>
   );
